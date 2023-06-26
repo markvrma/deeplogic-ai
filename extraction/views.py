83172d59286c69415a3ba9eef33a07738a05efc1
markvrma <markvrma@gmail.com>
2023-06-26 23:27:26 +0530

    basic form upload created

--- extraction/views.py
+++ extraction/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest
+from django.shortcuts import render
+from .forms import UploadFileForm
+from .extract import get_text
 
 # Create your views here.
+
+# def home(request):
+#     return render(request,"extraction/home")
+
+def home(request):
+    if request.method == "POST":
+        text = get_text(request.FILES["myfile"])
+        return render(request,'extraction/home.html',{"text":text})
+
+    # else:
+    #     return HttpResponseBadRequest()
+    
+    return render(request,'extraction/home.html') 
+
